310b365f21b5784c7c83b821a218d7e04da106f1
leekminxx <158007500+leekminxx@users.noreply.github.com>
2024-03-14 17:50:11 +0900

    고마워 소밍

---
+++ day01/enum.c
@@ -0,0 +1,13 @@
+
+// ¿­°ÅÇü
+#include <stdio.h>
+
+void main()
+{
+	enum season { SPRING = 10, SUMMER, FALL, WINTER } s;
+	s = SPRING;
+	printf("%d\n", s);
+
+	s = WINTER;
+	printf("%d\n", s);
+}
---
+++ day01/func01.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+void func()   // 함수 정의
+{
+	printf("func()\n");
+}
+void main()   // 함수 호출
+{
+	func();
+}
